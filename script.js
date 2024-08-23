211302dc4035289a98a0f3f026cba587359d3926
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-23 15:21:06 +0200

    MILESTONE 2

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('JS OK');
+
+// ! FASE DI PREPARAZIONE
+
+// Mi prendo gli elementi dal DOM
+const button = document.querySelector('button');
+const grid = document.getElementById('grid')
+
+// Funzione per creare le celle
+
+function getCell() {
+    // Creo una cella
+    const cell = document.createElement('div');
+    // Aggiungo la classe cell al div creato
+    cell.className = 'cell';
+    // Restituisco all'esterno la cella creata
+    return cell;
+}
+
+// ! EVENTI DINAMICI
+button.addEventListener('click', function(){
+
+// ! FASE DI ELABORAZIONE
+
+// Imposto il numero di celle come da traccia a 100
+const cells = 100;
+
+// Per ogni cella che voglio creare:
+for (let i = 0; i < cells; i++) {
+
+    // Creo una cella
+    const cell = getCell();
+
+    // Inserisco la cella nel DOM
+    grid.appendChild(cell);
+}
+})
