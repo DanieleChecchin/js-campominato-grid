8001983d5628e775eed4c6a673c7cf9b330e541e
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-23 15:54:22 +0200

    MILESTONE 4

--- script.js
+++ script.js
@@ -3,10 +3,9 @@
 
 // Mi prendo gli elementi dal DOM
 const button = document.querySelector('button');
-const grid = document.getElementById('grid')
+const grid = document.getElementById('grid');
 
 // Funzione per creare le celle
-
 function getCell(content) {
     // Creo una cella
     const cell = document.createElement('div');
@@ -15,28 +14,38 @@ function getCell(content) {
 
     // Aggiungo il contenuto alla cella (aggiungo i numeri chiesti)
     cell.append(content);
+
     // Restituisco all'esterno la cella creata
     return cell;
 }
 
 // ! EVENTI DINAMICI
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
 
-// ! FASE DI ELABORAZIONE
+    // ! FASE DI ELABORAZIONE
+    // Imposto il numero di celle come da traccia a 100
+    const cells = 100;
 
-// Imposto il numero di celle come da traccia a 100
-const cells = 100;
+    // Per ogni cella che voglio creare:
+    for (let i = 1; i <= cells; i++) {
 
-// Per ogni cella che voglio creare:
-for (let i = 1; i <= cells; i++) {
+        // Creo una cella (richiamando la funzione creata all'inizio)
+        const cell = getCell(i);
 
-    // Creo una cella (richiamando la funzione creata all'inizio)
-    const cell = getCell(i);
+        // Aggiungo il contenuto alla cella (richiamando la funzione creata e che ho modificato)
+        const content = cells[i];
 
-    // Aggiungo il contenuto alla cella (richiamando la funzione creata e che ho modificato)
-    const content = cells[i];
+        // Inserisco la cella nel DOM
+        grid.appendChild(cell);
 
-    // Inserisco la cella nel DOM
-    grid.appendChild(cell);
-}
+        // Aggiungo l'evento del click sulla cella
+        cell.addEventListener('click', function(){
+            // Aggiungo la classe che mi colora la cella al click
+            cell.classList.add('bg-cell');
+            //Stampo in console il numero della cella cliccata
+            console.log(i);
+        })
+    }
+
+  
 })
